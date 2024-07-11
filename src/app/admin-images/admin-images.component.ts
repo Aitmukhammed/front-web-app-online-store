fa8ec0214d9bef76f3510f0a53c8505eb625f9ba
Aitmukhammed <aitmukhammed.04@gmail.com>
2024-07-11 14:53:11 +0500

    Добавил компонент картинок для рекламы

--- src/app/admin-images/admin-images.component.ts
+++ src/app/admin-images/admin-images.component.ts
@@ -2,20 +2,76 @@ import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../service/ImageService';
 import { ImageDetails } from '../details/image-details.interface';
 import { error } from 'console';
+import { TokenStorageService } from '../auth/token-storage.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateImageDialogComponent } from '../update-image-dialog/update-image-dialog.component';
+
 
 @Component({
   selector: 'app-admin-images',
   templateUrl: './admin-images.component.html',
-  styleUrl: './admin-images.component.css'
+  styleUrl: './admin-images.component.css',
 })
 export class AdminImagesComponent implements OnInit {
   images: ImageDetails[] = [];
-  constructor(private imageService: ImageService) {
-
+  btnBool: boolean = false;
+  constructor(
+    private imageService: ImageService,
+    private tokenStorage: TokenStorageService,
+    public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.getAllImages();
+  }
+
+  toggleButtonAndFetchImages() {
+    this.btnBool = !this.btnBool;
+    this.getAllImages();
+  }
+
+  openUpdateDialog(image: ImageDetails): void {
+    const dialogRef = this.dialog.open(UpdateImageDialogComponent, {
+      width: '400px',
+      data: { image: image }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.updateImage(image.id, result);
+      }
+    });
+  }
+
+  onSubmit(formData: any, imageForm: any) {
+    if (imageForm.invalid) {
+      console.error('Form is invalid');
+      return;
+    }
+
+    const token = this.tokenStorage.getToken();
+    if (!token) {
+      console.error('Токен отсутствует, пользователь не аутентифицирован.');
+      return;
+    }
+
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    };
+
+    this.imageService.addImage(formData, headers).subscribe(
+      response => {
+        console.log('Product added successfully:', response);
+        imageForm.reset(); // Очистить форму
+        imageForm.submitted = false; // Сбросить состояние отправки
+      },
+      error => {
+        console.error('Error adding product:', error);
+        console.error('Headers: ', headers);
+        console.error('Form Data: ', formData);
+      }
+    );
   }
 
   getAllImages(): void {
@@ -28,6 +84,16 @@ export class AdminImagesComponent implements OnInit {
           console.error("Error fetching images: ", error);
         }
       )
+  }
+
+  updateImage(imageId: number, updatedImageData: any): void {
+    this.imageService.updateImage(imageId, updatedImageData)
+      .subscribe(() => {
+        this.getAllImages(); // Лучше вызывать только applyFilter(), но если нужно перезагрузить все продукты...
+      },
+        error => {
+          console.error('Error updating image: ', error);
+        });
   }
 
   deleteImage(imageId: number): void {
